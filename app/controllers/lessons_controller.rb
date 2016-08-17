3dfb7d1504fa532657310ff581578d0482d9a19d
Dan <danielpowell12@msn.com>
2016-08-17 15:21:57 -0600

    gotta enroll to view lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user
+  before_action :current_user_enrolled_in_course?
+
   def show
   end
-  
+
   private
+
+  def current_user_enrolled_in_course?
+    if current_user.enrolled_in?(current_lesson.section.course) == false
+      redirect_to course_path(current_lesson.section.course), :alert => 'You must enroll to view the lesson'
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
