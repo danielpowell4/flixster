0985b102f27f00c0d0fbe57ba3ddc9d472aae49b
Dan <danielpowell12@msn.com>
2016-08-19 15:58:19 -0600

    move add new section to modal and clean code

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -14,7 +14,7 @@ class EnrollmentsController < ApplicationController
       charge = Stripe::Charge.create(
         :customer    => customer.id,
         :amount      => @amount,
-        :description => 'Flixter Premo Content',
+        :description => 'Flixster Premo Content',
         :currency    => 'usd'
       )
     end
