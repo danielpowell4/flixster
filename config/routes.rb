88d2cf5741a17011691866b4a103fe856ae8197e
Dan <danielpowell12@msn.com>
2016-08-19 17:18:33 -0600

    wrap new lesson jQuery in function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'static_pages#index'
-  get 'privacy', to: 'static_pages#privacy'
   resource :dashboard, only: [:show]
+  get 'privacy', to: 'static_pages#privacy'
+  root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
